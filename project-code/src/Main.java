e2ae3975d96e7df3e1dc4cac339e6120a15ac385
Lokansh <lk527476@dal.ca>
2021-11-25 11:53:12 -0400

    Module 2 - insert query code

--- project-code/src/Main.java
+++ project-code/src/Main.java
@@ -10,13 +10,21 @@ public class Main {
         // accept user input
         String userArgument = null;
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter Query-------");
         userArgument = s.nextLine();
         userArgument = userArgument.trim();
         System.out.println("Input query is:" + userArgument);
+
         QueryOperations obj = new QueryOperations();
         //obj.createDatabase(userArgument);
-        //globalDBDirectoryPath = obj.useDatabase(userArgument);
-        //System.out.println("globalDBDirectoryPath->" + globalDBDirectoryPath);
-        obj.selectTableQuery(userArgument);
+        globalDBDirectoryPath = obj.useDatabase(userArgument);
+        System.out.println("globalDBDirectoryPath->" + globalDBDirectoryPath);
+
+        System.out.println("Enter Insert query-------");
+        String userArgument2 = s.nextLine();
+
+        obj.insertQuery(userArgument2,globalDBDirectoryPath);
+
+        //obj.selectTableQuery(userArgument);
     }
 }
