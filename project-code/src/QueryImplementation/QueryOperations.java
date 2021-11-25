ca41dff39004a41869daff71f6138dcc49a5b4d5
MSI\nikit <nikita.kothari@dal.ca>
2021-11-25 10:04:30 +0530

    select with where clause feature added

--- project-code/src/QueryImplementation/QueryOperations.java
+++ project-code/src/QueryImplementation/QueryOperations.java
@@ -74,27 +74,54 @@ public class QueryOperations {
         String database = null;
         String table = null;
         String providedColumns = null;
+        String provWhereClause = null;
+        String tablename = null;
         Formatter fmt = new Formatter();
 
+        // converting query to lower case
+        //Query = Query.toLowerCase();
+
         // Logic to extract table name and database name
-        String tablename = removeSemiColon(Query.substring(Query.indexOf("from") + 4).trim());
+        Pattern patternDBTable;
+        if (Query.contains("where")){
+            patternDBTable = Pattern.compile("(?<=from)(.*)(?=where)", Pattern.DOTALL);
+        } else {
+            patternDBTable = Pattern.compile("(?<=from)(.*)(?=;|)", Pattern.DOTALL);
+        }
+        Matcher matcherDBTable = patternDBTable.matcher(Query);
+        while (matcherDBTable.find()) {
+            tablename = removeSemiColon(matcherDBTable.group(0).trim());
+        }
+        System.out.println("TableName: "+tablename);
+
+        //String tablename = removeSemiColon(Query.substring(Query.indexOf("from") + 4).trim());
         if (tablename.contains(".")){
             database=tablename.split("\\.")[0];
             table=tablename.split("\\.")[1];
         }
         String tabledataStoragePath = dataStoragePath+database+"/"+table+"/";
+        System.out.println("Table data location: "+tabledataStoragePath);
+
+        // Logic to check if where clause is present
+        Pattern patternWhereClause = Pattern.compile("(?<=where).*$", Pattern.DOTALL);
+        Matcher matcherWhere = patternWhereClause.matcher(Query);
+        while (matcherWhere.find()) {
+            provWhereClause = matcherWhere.group(0).trim();
+        }
+        System.out.println("Filter Condition: "+provWhereClause);
 
         // Logic to extract columns
-        Pattern pattern = Pattern.compile("select(.*?)from", Pattern.DOTALL);
-        Matcher matcher = pattern.matcher(Query);
-        while (matcher.find()) {
-            providedColumns = matcher.group(1).trim();
+        Pattern patternColumns = Pattern.compile("select(.*?)from", Pattern.DOTALL);
+        Matcher matcherColumns = patternColumns.matcher(Query);
+        while (matcherColumns.find()) {
+            providedColumns = matcherColumns.group(1).trim();
         }
-        System.out.println(providedColumns);
+        System.out.println("Columns Required: "+providedColumns);
+
         List<String> providedColumnsList = Arrays.asList(providedColumns.split(","));
 
         // Logic to get data from the generated path for the required columns
-        List<String> data = readFile(tabledataStoragePath, providedColumns);
+        List<String> data = readFile(tabledataStoragePath, providedColumns, provWhereClause);
 
         int colsLength = data.get(0).split(",").length;
         int counter=0;
@@ -112,10 +139,12 @@ public class QueryOperations {
     }
 
     // Read and Process data file
-    public List<String> readFile(String path, String columns) {
+    public List<String> readFile(String path, String columns, String filter) {
         try {
             // initialization
             String st;
+            Pattern patternWhereKey = null;
+            Pattern patternWhereValue = null;
             List<Integer> indexList = new ArrayList<>();
             List<String> appendData = new ArrayList<>();
 
@@ -127,6 +156,24 @@ public class QueryOperations {
             String header = br.readLine();
             List<String> headercolsList = Arrays.asList(header.split(","));
             int fileColumnsLen = headercolsList.size();
+
+            // read filter column position
+            int filterColPos = 0;
+            String filterColumn=null;
+            String filterValue=null;
+            if (filter != null) {
+                patternWhereKey = Pattern.compile("^[^=]+", Pattern.DOTALL);
+                patternWhereValue = Pattern.compile("(?<==)(.*)(?=|;)", Pattern.DOTALL);
+                Matcher matcherWhereKey = patternWhereKey.matcher(filter);
+                Matcher matcherWhereValue = patternWhereValue.matcher(filter);
+                while (matcherWhereKey.find()) {
+                    filterColumn = matcherWhereKey.group(0).trim();
+                }
+                while (matcherWhereValue.find()) {
+                    filterValue = removeSemiColon(matcherWhereValue.group(0).trim()).replace("\"", "");;
+                }
+                filterColPos = headercolsList.indexOf(filterColumn);
+            }
 
             // read given columns
             int provColumnsLen = fileColumnsLen;
@@ -158,10 +205,23 @@ public class QueryOperations {
             while ((st = br.readLine()) != null) {
                 String[] splitSt = st.split(",");
                 String reqString = "";
-                for (int t: indexList){
-                    reqString+=splitSt[t]+",";
+                if (filter == null) {
+                    // when select without where clause
+                    for (int t : indexList) {
+                        reqString += splitSt[t] + ",";
+                    }
+                    appendData.add(reqString.substring(0, reqString.length() - 1));
                 }
-                appendData.add(reqString.substring(0, reqString.length() - 1));
+                else {
+                    // when select with where clause
+                    if (splitSt[filterColPos].equals(filterValue)){  // XXX hardcoded needs to be removed
+                        // when select without where clause
+                        for (int t : indexList) {
+                            reqString += splitSt[t] + ",";
+                        }
+                        appendData.add(reqString.substring(0, reqString.length() - 1));
+                    }
+                }
             }
             return appendData;
         } catch (IOException e) {System.out.println(Arrays.toString(e.getStackTrace()));}
