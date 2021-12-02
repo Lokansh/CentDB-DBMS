836796424a0c0774e4d088a78d20a35acc027451
MSI\nikit <nikita.kothari@dal.ca>
2021-12-02 13:09:01 +0530

    updated code flow for Module 2

--- project-code/src/Main.java
+++ project-code/src/Main.java
@@ -4,9 +4,11 @@ import QueryImplementation.UseQuery;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static String globalDBDirectoryPath = "";//Static global database directory address
+    public static String globalDBDirectoryPath = "";    //Static global database directory address
 
     public static void main(String[] args) throws IOException {
         // accept user input
@@ -18,25 +20,72 @@ public class Main {
         System.out.println("Input query is:" + userArgument);
 
         QueryOperations obj = new QueryOperations();
-        UseQuery useQueryObj = new UseQuery();
-        globalDBDirectoryPath = useQueryObj.useDatabase(userArgument);
-        System.out.println("globalDBDirectoryPath->" + globalDBDirectoryPath);
-        obj.createSchema("create table table1 (id int(10),name varchar(25));",globalDBDirectoryPath);
-        // obj.selectTableQuery(userArgument);
 
+        // Pattern Matcher for CREATE DATABASE
+        Pattern createDBPattern = Pattern.compile("^create database.*$", Pattern.DOTALL);
+        Matcher createDBMatcher = createDBPattern.matcher(userArgument);
+        if (createDBMatcher.find()) {
+            System.out.println(createDBMatcher.group(0).trim());
+            obj.createDatabase(userArgument);
+        }
 
-        //obj.createDatabase(userArgument);
-        globalDBDirectoryPath = useQueryObj.useDatabase(userArgument);
-        System.out.println("globalDBDirectoryPath->" + globalDBDirectoryPath);
+        // Pattern Matcher for USE
+        Pattern usePattern = Pattern.compile("^use database.*$", Pattern.DOTALL);
+        Matcher useMatcher = usePattern.matcher(userArgument);
+        if (useMatcher.find()) {
+            System.out.println(useMatcher.group(0).trim());
+            UseQuery useQueryObj = new UseQuery();
+            globalDBDirectoryPath = useQueryObj.useDatabase(userArgument);
+            System.out.println("globalDBDirectoryPath->" + globalDBDirectoryPath);
+        }
 
-        System.out.println("Enter Insert query-------");
-        String userArgument2 = s.nextLine();
+        // Pattern Matcher for CREATE TABLE
+        Pattern createTBPattern = Pattern.compile("^create table.*$", Pattern.DOTALL);
+        Matcher createTBmatcher = createTBPattern.matcher(userArgument);
+        if (createTBmatcher.find()) {
+            System.out.println(createTBmatcher.group(0).trim());
+            //obj.createSchema("create table table1 (id int(10),name varchar(25));",globalDBDirectoryPath);
+            obj.createSchema(userArgument, globalDBDirectoryPath);
+        }
 
-        InsertQuery insertQueryObj = new InsertQuery();
-        insertQueryObj.insertQuery(userArgument2,globalDBDirectoryPath);
+        // Pattern Matcher for INSERT INTO TABLE
+        Pattern insertPattern = Pattern.compile("^insert into table.*$", Pattern.DOTALL);
+        Matcher insertMatcher = insertPattern.matcher(userArgument);
+        if (insertMatcher.find()) {
+            System.out.println(insertMatcher.group(0).trim());
+            String userArgument2 = s.nextLine();
 
-        //obj.selectTableQuery(userArgument);
-        
+            InsertQuery insertQueryObj = new InsertQuery();
+            insertQueryObj.insertQuery(userArgument2,globalDBDirectoryPath);
+        }
 
+        // Pattern Matcher for SELECT
+        Pattern selectPattern = Pattern.compile("^select.*$", Pattern.DOTALL);
+        Matcher selectMatcher = selectPattern.matcher(userArgument);
+        if (selectMatcher.find()) {
+            System.out.println(selectMatcher.group(0).trim());
+            obj.selectTableQuery(userArgument);
+        }
+
+        // Pattern Matcher for UPDATE
+        Pattern updatePattern = Pattern.compile("^use.*$", Pattern.DOTALL);
+        Matcher updateMatcher = updatePattern.matcher(userArgument);
+        if (updateMatcher.find()) {
+            System.out.println(updateMatcher.group(0).trim());
+        }
+
+        // Pattern Matcher for DELETE
+        Pattern deletePattern = Pattern.compile("^delete.*$", Pattern.DOTALL);
+        Matcher deleteMatcher = deletePattern.matcher(userArgument);
+        if (deleteMatcher.find()) {
+            System.out.println(deleteMatcher.group(0).trim());
+        }
+
+        // Pattern Matcher for DROP
+        Pattern dropPattern = Pattern.compile("^drop table.*$", Pattern.DOTALL);
+        Matcher dropMatcher = dropPattern.matcher(userArgument);
+        if (dropMatcher.find()) {
+            System.out.println(dropMatcher.group(0).trim());
+        }
     }
 }
