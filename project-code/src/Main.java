1f815b1a922b294de5fd33f50fe90bc733125667
MSI\nikit <nikita.kothari@dal.ca>
2021-12-03 13:16:05 +0530

    update logic without specifying database

--- project-code/src/Main.java
+++ project-code/src/Main.java
@@ -5,29 +5,13 @@ import authentication.registration.UserRegistration;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         Session userSession = Session.getInstance();
-//        accept user input
-//        String userArgument = null;
-//        userArgument = scanner.nextLine();
-//        userArgument = userArgument.trim();
-//        System.out.println(userArgument);
-//
-//
-//        // "create database" to be replaced with PATTERN MATCHER : Lokansh working on the smae
-//
-//        if (userArgument.contains("create database")) {
-//            String dbname = userArgument.replace("create database", "");
-//            dbname = dbname.trim();
-//            CentDB dbobj = new CentDB();
-//            String db_filename = "datafiles/" + dbname + ".txt";
-//            String writebackStr = dbname + ";";
-//            String writebackType = "create database";
-//            dbobj.writeback(db_filename, writebackStr, writebackType);
-//        }
 
         while (true) {
             System.out.println("1. User Registration");
